574054c87415317d7a86951b3720a6cb95bc2699
Rhys Campbell <rhys.james.campbell@googlemail.com>
2019-11-19 17:29:04 +0100

    Add flask app stuff

--- DockerCompose/README.md
+++ DockerCompose/README.md
@@ -1,12 +1,18 @@
 # Docker Compose Notes
 
+To build the custom images...
+
+```
+docker-compose build
+```
+
 To start the setup
 
 ```
 docker-compose up -d
 ```
 
-To destroy the setup including volumes 
+To destroy the setup including volumes
 
 ```
 docker-compose down -v
---
+++ DockerCompose/app/app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import request
+from flask import render_template
+
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'secret'
+app.config['MYSQL_DB'] = 'ukpostcodes'
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET'])
+def index():
+  postcode = request.args.get("postcode")
+  MYSQL_HOST = "dockercompose_db_1"
+  MYSQL_USER = "root"
+  MYSQL_PASSWORD = "secret"
+  if postcode is not None:
+      cur = mysql.connection.cursor()
+      cur.execute("SELECT * FROM postcodelatlng WHERE postcode = %s", postcode)
+      rv = cur.fetchall()
+      payload = []
+      content = {}
+      for result in rv:
+          content = {'id': result[0], 'postcode': result[1], 'longitude': result[2], 'latitude': result[2]}
+          payload.append(content)
+          content = {}
+      return jsonify(payload)
+
+  else:
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
